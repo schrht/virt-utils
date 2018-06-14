600bf0e9d76e6122c098f5c8f595535452a8292d
Charles Shih <shichen120@gmail.com>
2018-06-14 17:19:56 +0800

    Support HTML report as dumpped file and email notification.

--- aws_resource_keeper/arkeeper.py
+++ aws_resource_keeper/arkeeper.py
@@ -7,6 +7,8 @@ History:
 v1.0    2018-06-13  charles.shih  Finish basic the functions
 v1.1    2018-06-14  charles.shih  Read user configuration from yaml file
 v1.2    2018-06-14  charles.shih  Support email notificaiton
+v1.3    2018-06-14  charles.shih  Support HTML report as dumpped file
+v1.4    2018-06-14  charles.shih  Support HTML report as email notification
 """
 
 import boto3
@@ -105,15 +107,17 @@ class AwsResourceCollector():
 class AwsResourceReporter():
     """Reporter with html and email notification supported."""
 
-    smtp_server = 'smtp.corp.redhat.com'
+    smtp_server = 'smtp.example.com'
     smtp_port = '25'
     smtp_user = smtp_pass = ''
-    sender = 'cheshi@redhat.com'
-    receivers = ['cheshi@redhat.com']
+    sender = 'name@example.com'
+    receivers = ['name@example.com']
     email_subject = '[TEST] AWS Resource Report'
 
+    html_report = ''
+
     def __init__(self):
-        """Read user configuration from yaml file."""
+        """Read user configuration."""
         try:
             with open(os.path.expanduser('~/.arkeeper.yaml'), 'r') as f:
                 yaml_dict = yaml.load(f)
@@ -142,13 +146,15 @@ class AwsResourceReporter():
                 if 'EmailSubject' in user_config:
                     self.email_subject = user_config['EmailSubject']
 
+            with open('./report_template.html', 'r') as f:
+                self.html_report = f.read()
+
         except Exception as err:
             print 'WARNING: encounter an error while parsing user config.'
             print err
 
-    def send_email(self, mail_msg='Message body...'):
+    def send_email(self, mail_msg='Message body...', subtype='plain'):
         """Send out the report as email notification."""
-        subtype = 'plain'  # 'html' or 'plain'
         message = MIMEText(mail_msg, subtype, 'utf-8')
         message['Subject'] = Header(self.email_subject, 'utf-8')
         message['From'] = Header(self.sender, 'ascii')
@@ -161,8 +167,35 @@ class AwsResourceReporter():
                 smtpObj.login(self.smtp_user, self.smtp_pass)
             smtpObj.sendmail(self.sender, self.receivers, message.as_string())
             print 'NOTE: Email notification sent!'
-        except smtplib.SMTPException:
+        except smtplib.SMTPException as err:
             print 'ERROR: error while sending email notification.'
+            print err
+
+    def html_append(self, content):
+        """Append content to the html report."""
+        # The content should be inserted before '</body>'
+        origin = self.html_report
+        self.html_report = origin.replace('</body>', content + '\n</body>', 1)
+
+    def html_get_string(self):
+        """Get html report as string."""
+        return self.html_report
+
+    def html_dump(self, file='./report.html'):
+        """Dump html report as an html file."""
+        try:
+            with open(file, 'w') as f:
+                f.write(self.html_report)
+        except Exception as err:
+            print 'ERROR: error while dumping html file.'
+            print err
+
+        print 'NOTE: html file dumpped! (%s)' % file
+
+
+    def html_send(self):
+        """Send html report as an Email notification."""
+        self.send_email(mail_msg=self.html_report, subtype='html')
 
 
 if __name__ == "__main__":
@@ -172,4 +205,12 @@ if __name__ == "__main__":
     # print table
 
     reporter = AwsResourceReporter()
-    reporter.send_email()
+    my_text = """\
+    This is the content...
+    """
+    reporter.html_append(my_text)
+    reporter.html_append(my_text)
+    reporter.html_append(my_text)
+
+    reporter.html_dump()
+    reporter.html_send()
