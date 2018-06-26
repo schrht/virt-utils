d0c1a22f839066eddb15f39406d23c535476d2a8
Charles Shih <shichen120@gmail.com>
2018-06-26 09:42:10 +0800

    Modify the words in HTML report.

--- aws_resource_keeper/arkeeper.py
+++ aws_resource_keeper/arkeeper.py
@@ -12,6 +12,7 @@ v1.4    2018-06-14  charles.shih  Support HTML report as email notification
 v1.4.1  2018-06-25  charles.shih  Bugfix for no unused resource found
 v1.5    2018-06-25  charles.shih  Get available regions when they not specified
 v1.5.1  2018-06-25  charles.shih  Bugfix for generating html report
+v1.5.2  2018-06-26  charles.shih  Modify the words in HTML report
 """
 
 import boto3
@@ -234,9 +235,9 @@ if __name__ == "__main__":
             '<h1 style="color:red">Did you forget something?</h1>')
         reporter.html_append('<h2>Running Instance</h2>')
         reporter.html_append(table)
-        reporter.html_append('<h3>Searched Regions: %s</h3>' % str.join(
+        reporter.html_append('<h3>Search in: %s</h3>' % str.join(
             ', ', collector.region_list))
-        reporter.html_append('<h3>Filtered Keynames: %s</h3>' % str.join(
+        reporter.html_append('<h3>Filter by: %s</h3>' % str.join(
             ', ', collector.keyname_list))
 
         reporter.html_dump()
