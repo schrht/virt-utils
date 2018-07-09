42f93e0247222cb6409a7fd1be530fb068b9abbe
Charles Shih <shichen120@gmail.com>
2018-07-09 14:41:37 +0800

    Update the test logic in cpu_test.sh

--- cpu_tests/cpu_test.sh
+++ cpu_tests/cpu_test.sh
@@ -11,22 +11,18 @@ frequence_boost() {
 	if [ "$label" = "enable" ]; then
 		# Enable frequence boost (turbo mode)
 		if [ -e /sys/devices/system/cpu/cpufreq/boost ]; then
-			cmd="echo 1 > /sys/devices/system/cpu/cpufreq/boost"
-			run $cmd
+			run "echo 1 > /sys/devices/system/cpu/cpufreq/boost"
 		elif [ -e /sys/devices/system/cpu/intel_pstate/no_turbo ]; then
-			cmd="echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo"
-			run $cmd
+			run "echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo"
 		else
 			echo "Can not enable frequence boost."
 		fi
 	else
 		# Disable frequence boost (turbo mode)
 		if [ -e /sys/devices/system/cpu/cpufreq/boost ]; then
-			cmd="echo 0 > /sys/devices/system/cpu/cpufreq/boost"
-			run $cmd
+			run "echo 0 > /sys/devices/system/cpu/cpufreq/boost"
 		elif [ -e /sys/devices/system/cpu/intel_pstate/no_turbo ]; then
-			cmd="echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
-			run $cmd
+			run "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
 		else
 			echo "Can not disable frequence boost."
 		fi
@@ -38,20 +34,22 @@ frequence_boost() {
 
 date
 
-cmd="lscpu"
-run $cmd
+run "lscpu"
 
 frequence_boost enable
 
-for i in {1,2,4,8,12,16,20,24}; do
-	cmd="turbostat --show Core,CPU,Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,sysfs --quiet taskset -c 0-$((i-1)) stress -c $i -t 10"
-	run $cmd
+# The On-line CPU must be shown in "A-B" format
+start_num=$(lscpu | grep "On-line CPU(s) list:" | cut -d: -f2 | cut -d- -f1 | tr -d " ")
+
+for i in {1..24}; do
+	: ${start_num:=0}
+	stop_num=$((start_num+i-1))
+	run "turbostat --show Core,CPU,Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,sysfs --quiet taskset -c ${start_num}-${stop_num} stress -c $i -t 10"
 done
 
 frequence_boost disable
 
-cmd="turbostat --show Core,CPU,Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,sysfs --quiet taskset -c 0-$((i-1)) stress -c $i -t 10"
-run $cmd
+run "turbostat --show Core,CPU,Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,sysfs --quiet taskset -c ${start_num}-${stop_num} stress -c $i -t 10"
 
 frequence_boost enable
 
